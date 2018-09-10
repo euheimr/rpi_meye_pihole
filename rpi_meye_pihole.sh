aa0d5665721fb9366e9850264bc8196d77072939
Jacob Betz <12131909+jacobbetz@users.noreply.github.com>
2018-09-09 20:55:10 -0700

    initial commit!

---
+++ rpi_meye_pihole.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# rpi_meye_pihole.sh - Pi-hole & Motioneye Service auto-install script
+# ---------------------------------------------------------------------------
+# 	
+# 	!!!IMPORTANT!!!
+#	THIS SCRIPT DOES NOT SUPPORT DOCKER!
+#
+
+#TODO: Check or ask here to ask if docker or not, or check for it?
+
+#PI-HOLE AD BLOCKER
+#	https://pi-hole.net/
+#
+#MOTION EYE:
+#	https://github.com/ccrisan/motioneyeos/wiki/Configuration
+#
+# 	Author: Jacob Betz
+#	https://github.com/jacobbetz
+#	
+#
+# 	Revision history:
+# 	9/09/2018 Created the script
+# ---------------------------------------------------------------------------
+
+#run in sudo
+sudo -i
+
